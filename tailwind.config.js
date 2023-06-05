db159ca961298d9c7f4c8c5ce2a710c8bb21567b
Yogesh <yogesh7132@gmail.com>
2023-06-05 15:15:27 +0530

    Genesis: Bank App

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,22 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-sarif"]
+      }
+    },
     screens: {
       xs: "480px",
       sm: "640px",
       md: "768px",
-      lg: "1024x",
+      lg: "1024px",
       xl: "1280px",
       "2xl": "1536px"
     }
